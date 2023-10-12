614d38b6a2b460fcc19b6a0ecda7222476344f1f
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-12 15:11:56 +0700

    Add files via upload

---
+++ Day4ade.java
@@ -0,0 +1,18 @@
+package day4ade;
+
+public class Day4ade {
+
+    public static void main(String[] args) {
+        int a,b,c,saya;
+        
+        a = 20;
+        b = 10;
+        c = 40;
+        
+        saya = a + b + c;
+        
+        System.out.println("nilai adee :"+saya);
+
+    }
+
+}
