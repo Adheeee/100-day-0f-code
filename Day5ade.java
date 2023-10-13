bc32a257ca442ae2d604b16ba5b09fd4628f32c3
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-13 03:06:44 -0700

    Add files via upload

---
+++ Day5ade.java
@@ -0,0 +1,22 @@
+package day5ade;
+
+import java.util.Scanner;
+
+public class Day5ade {
+
+    public static void main(String[] args) {
+        int a,b,c,ade;
+        
+        Scanner adee = new Scanner(System.in);
+        
+        a = adee.nextInt();
+        b = adee.nextInt();
+        c = adee.nextInt();
+        
+        ade = a * b - c ;
+        
+        System.out.println("tampikan semua nilainya:"+ade);
+        }
+
+    }
+
