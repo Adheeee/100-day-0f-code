5d8959cf0ab50d492c3f44a4e2f1b0fda72e91d5
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-16 21:08:24 +0700

    Update day8ade.java

--- day8ade.java
+++ day8ade.java
@@ -3,7 +3,7 @@ public class day8ade {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i--) {
-            System.out.println(i + i);
+            System.out.println(i);
             
         }
     }
