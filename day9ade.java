e91f9f4c89a2cd6f835735017a58689e93d8529d
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-17 19:47:06 +0700

    Add files via upload

---
+++ day9ade.java
@@ -0,0 +1,12 @@
+
+public class day9ade {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("adeeeeee");
+            
+        }
+            
+        }
+    }
+
