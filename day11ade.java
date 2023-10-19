9b4d9d02c150d663225334e00a9e5b21394305d5
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-19 20:53:46 +0700

    Add files via upload

---
+++ day11ade.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day11ade {
+  
+    public static void main(String[] args) {
+       
+       Scanner input=new Scanner(System.in);
+       int bilangan=0;
+
+        while (bilangan!=10){
+            System.out.print("Masukkan Bilangan: ");
+            bilangan=input.nextInt(); 
+    }
+
+}
+}
