3a46acc52c419066e9c81c5b11b9b742a61039c4
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-21 20:11:08 +0800

    Add files via upload

---
+++ Day13ade.java
@@ -0,0 +1,24 @@
+public class Main {
+	public static void main(String[] args) {
+		int []data = new int [6];
+		
+		data[0] = 2;
+		data[1] = 3;
+		data[2] = 6;
+		data[3] = 9;
+		data[4] = 8;
+		data[5] = 4;
+		
+		System.out.println(data[0]);
+		
+		System.out.println(data[1]);
+		
+		System.out.println(data[2]);
+		
+		System.out.println(data[3]);
+		
+		System.out.println(data[4]);
+		
+		System.out.println(data[5]);
+	}
+}
