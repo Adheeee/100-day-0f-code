7a90f840f627a29d6eb7cd30c6db550616f0b30a
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-10-23 00:57:28 +0800

    Add files via upload

---
+++ Day15ade.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+	
+	int angka = input.nextInt();
+	
+	int hasil = angka - 2;
+	
+	System.out.println(hasil);
+	
+	
+	
+	}
+}
