4ad1e85fe83e811add2a8895b61007192e127da3
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-11-09 21:55:08 +0700

    Add files via upload

---
+++ Day32ade.java
@@ -0,0 +1,15 @@
+
+public class Day32ade {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Iterasi ke-" + i);
+        }
+
+        int angka = 1;
+        while (angka <= 20) {
+            System.out.println("Angka: " + angka);
+            angka++;
+        }
+    }
+}
