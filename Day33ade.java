2aade704c8b45dd6979ae2ef3a9fc8a10dcc5603
Adheeee <147314638+Adheeee@users.noreply.github.com>
2023-11-09 23:03:49 +0700

    Add files via upload

---
+++ Day33ade.java
@@ -0,0 +1,12 @@
+
+public class Day33ade {
+
+    public static void main(String[] args) {
+        int hasil = penambahan(20, 20);
+        System.out.println("Hasil tambah : " + hasil);
+    }
+
+    static int penambahan(int a, int b) {
+        return a + b;
+    }
+}
